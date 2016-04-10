2a8f45d333784a9536ff68179b7faa7755080d66
Oleksii Fedorov <waterlink000@gmail.com>
2016-04-10 03:30:58 +0100

    Wire up ViewController with TipCalculatorModel

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -6,32 +6,68 @@ import UIKit
 
 extension UITextField: Editable {}
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, TipCalculatorModelObserver {
     @IBOutlet weak var totalTextField: Editable!
     @IBOutlet weak var taxPercentageSlider: UISlider!
     @IBOutlet weak var taxPercentageLabel: UILabel!
     @IBOutlet weak var resultsTextView: UIView!
     
-    var tipCalculatorModel: TipCalculatorModel!
+    var tipCalculatorModel: TipCalculatorModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if tipCalculatorModel == nil {
+            tipCalculatorModel = TipCalculatorModel(
+                total: 0,
+                taxPercentage: CalculateTaxPercentageValue()
+            )
+            tipCalculatorModel?.subscribe(self)
+        }
+    }
 
     @IBAction func handleTap(sender: AnyObject) {
         totalTextField.endEditing(true)
     }
 
     @IBAction func handleTaxPercentageChange(sender: AnyObject) {
-        tipCalculatorModel.taxPercentage = taxPercentageValue()
+        tipCalculatorModel?.taxPercentage = CalculateTaxPercentageValue()
     }
     
-    private func taxPercentageValue() -> Double {
-        return taxPercentageSliderCurrentValue() / taxPercentageSliderMaxValue()
+    func handleTipCalculatorModelChange(model: TipCalculatorModel) {
+        taxPercentageLabel.text = TipCalculatorPresenter(model: model).presentTaxPercentageLabel()
     }
     
-    private func taxPercentageSliderMaxValue() -> Double {
+    private func CalculateTaxPercentageValue() -> Double {
+        return fetchTaxPercentageSliderCurrentValue() / fetchTaxPercentageSliderMaxValue()
+    }
+    
+    private func fetchTaxPercentageSliderMaxValue() -> Double {
         return Double(taxPercentageSlider.maximumValue)
     }
     
-    private func taxPercentageSliderCurrentValue() -> Double {
+    private func fetchTaxPercentageSliderCurrentValue() -> Double {
         return Double(taxPercentageSlider.value)
+    }
+}
+
+class TipCalculatorPresenter {
+    let model: TipCalculatorModel
+    
+    init(model: TipCalculatorModel) {
+        self.model = model
+    }
+    
+    func presentTaxPercentageLabel() -> String {
+        return "Tax Percentage (\(presentTaxPercentage()))"
+    }
+    
+    func presentTaxPercentage() -> String {
+        return "\(calculateReadableTaxPercetage())%"
+    }
+    
+    private func calculateReadableTaxPercetage() -> Int {
+        return Int(model.taxPercentage * 100)
     }
 }
 
--- TipCalculatorTests/ViewControllerSpecs.swift
+++ TipCalculatorTests/ViewControllerSpecs.swift
@@ -8,6 +8,7 @@ class ViewControllerSpecs: QuickSpec {
     override func spec() {
         var totalTextField: MockEditable!
         var taxPercentageSlider: UISlider!
+        var taxPercentageLabel: UILabel!
         var tipCalculatorModel: TipCalculatorModel!
         var subject: ViewController!
         
@@ -18,6 +19,11 @@ class ViewControllerSpecs: QuickSpec {
         func slideTaxPercentage(to value: Float) {
             taxPercentageSlider.value = value
             subject.handleTaxPercentageChange(UISlider())
+        }
+        
+        func updateTaxPercentage(with value: Double) {
+            tipCalculatorModel.taxPercentage = value
+            subject.handleTipCalculatorModelChange(tipCalculatorModel)
         }
         
         func buildSlider() -> UISlider {
@@ -37,6 +43,7 @@ class ViewControllerSpecs: QuickSpec {
             viewController.totalTextField = totalTextField
             viewController.taxPercentageSlider = taxPercentageSlider
             viewController.tipCalculatorModel = tipCalculatorModel
+            viewController.taxPercentageLabel = taxPercentageLabel
             return viewController
         }
         
@@ -44,6 +51,7 @@ class ViewControllerSpecs: QuickSpec {
             totalTextField = MockEditable()
             tipCalculatorModel = buildTipCalculatorModel()
             taxPercentageSlider = buildSlider()
+            taxPercentageLabel = UILabel()
             subject = buildViewController()
         }
         
@@ -55,29 +63,75 @@ class ViewControllerSpecs: QuickSpec {
         }
         
         describe("-handleTaxPercentageChange(_:)") {
-            it("updates model's taxPercentage with min value") {
-                slideTaxPercentage(to: 0)
-                expect(modelTaxPercentage).to(beCloseTo(0))
-            }
-            
-            it("updates model's taxPercentage with some value") {
-                slideTaxPercentage(to: 1.9)
-                expect(modelTaxPercentage).to(beCloseTo(0.19))
-            }
-            
-            it("updates model's taxPercentage with max value") {
-                slideTaxPercentage(to: 10)
-                expect(modelTaxPercentage).to(beCloseTo(1))
-            }
-            
-            context("when slider's max value is different") {
-                beforeEach {
-                    taxPercentageSlider.maximumValue = 25
+            describe("model updates") {
+                it("sets taxPercentage to min value") {
+                    slideTaxPercentage(to: 0)
+                    expect(modelTaxPercentage).to(beCloseTo(0))
                 }
                 
-                it("updates TipCalculatorModel with some value") {
+                it("sets taxPercentage to some value") {
                     slideTaxPercentage(to: 1.9)
-                    expect(modelTaxPercentage).to(beCloseTo(1.9 / 25))
+                    expect(modelTaxPercentage).to(beCloseTo(0.19))
+                }
+                
+                it("sets taxPercentage to max value") {
+                    slideTaxPercentage(to: 10)
+                    expect(modelTaxPercentage).to(beCloseTo(1))
+                }
+                
+                context("when slider's max value is different") {
+                    beforeEach {
+                        taxPercentageSlider.maximumValue = 25
+                    }
+                    
+                    it("sets to some value") {
+                        slideTaxPercentage(to: 1.9)
+                        expect(modelTaxPercentage).to(beCloseTo(1.9 / 25))
+                    }
+                }
+            }
+        }
+        
+        describe("-handleTipCalculatorModelChange(_:)") {
+            describe("taxPercentage change") {
+                it("updates tax percentage label with 0% value") {
+                    updateTaxPercentage(with: 0)
+                    expect(taxPercentageLabel.text).to(equal("Tax Percentage (0%)"))
+                }
+                
+                it("updates tax percentage label with 100% value") {
+                    updateTaxPercentage(with: 1)
+                    expect(taxPercentageLabel.text).to(equal("Tax Percentage (100%)"))
+                }
+                
+                it("updates tax percentage label with 18% value") {
+                    updateTaxPercentage(with: 0.183)
+                    expect(taxPercentageLabel.text).to(equal("Tax Percentage (18%)"))
+                }
+            }
+        }
+        
+        describe("-viewDidLoad(_:)") {
+            context("when model is already created") {
+                it("does not change its model") {
+                    subject.viewDidLoad()
+                    expect(subject.tipCalculatorModel).to(be(tipCalculatorModel))
+                }
+            }
+            
+            context("when there is no model yet") {
+                beforeEach {
+                    subject.tipCalculatorModel = nil
+                    subject.viewDidLoad()
+                }
+                
+                it("does not change its model") {
+                    expect(subject.tipCalculatorModel).notTo(beNil())
+                }
+                
+                it("subscribes to the model") {
+                    subject.tipCalculatorModel?.taxPercentage = 0.75
+                    expect(taxPercentageLabel.text).to(equal("Tax Percentage (75%)"))
                 }
             }
         }
